7bee49db45df53b3d967998d59193365402148c3
Aurélien Arcangeli <arkaurel5929@gmail.com>
2023-01-01 19:11:30 +0100

    modif

--- src/pages/EmployeesList.jsx
+++ src/pages/EmployeesList.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import TableEmployees from "../components/TableEmployees";
 import "../styles/EmployeesList.css";
-import logo from "../assets/logo.png";
 import "../styles/App.css";
 
 export default function EmployeesList() {
     return (
         <div id="employee-div" className="container">
-            <img src={logo} alt="logo" />
             <h1>Current Employees</h1>
 
             <div className="body">
@@ -17,7 +15,7 @@ export default function EmployeesList() {
                 </section>
             </div>
 
-            <Link to={`/`} className="link">Home</Link>
+            <Link to={`/`} className="link" rel="preload">Home</Link>
         </div>
     )
 }
